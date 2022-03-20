1bc93d62fb77320a2363e0373aa7c5ba9284d068
Benjamin Durham <admin@bencey.co.nz>
2022-03-21 09:25:37 +1300

    Update usefulcommands.ps1

--- level6/usefulcommands.ps1
+++ level6/usefulcommands.ps1
@@ -1,3 +1,6 @@
 
+# Create Forest
+Install-ADDSForest -DomainName vdom.local -InstallDNS
+
 ## Add Domain User
 New-ADUser -Name "Benjamin Durham" -GivenName "Benjamin" -Surname "Durham" -SamAccountName "Ben" -UserPrincipalName "Ben@contoso.local" -AccountPassword(Read-Host -AsSecureString "Enter Password") -Enabled $true
