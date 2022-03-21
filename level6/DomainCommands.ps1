61ced6f2a290286c7f79bbaf86269a55026c2bee
Benjamin Durham <admin@bencey.co.nz>
2022-03-22 11:27:10 +1300

    Update DomainCommands.ps1

--- level6/DomainCommands.ps1
+++ level6/DomainCommands.ps1
@@ -1,1 +1,5 @@
 Add-Computer -DomainName adatum -Credential (Get-Credential) -NewName LON-SVR1 -restart
+
+
+# Add second Domain Controller
+Install-ADDSDomainController -domain contoso.local -SafeModeAdministratorPassword ("P@ssw0rd" | ConvertTo-SecureString -AsPlainText -force) -Credential (Get-Credential)
