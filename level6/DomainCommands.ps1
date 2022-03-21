d8ec5f0c38f15d24d70ab8130a30f2ea90ddb161
Benjamin Durham <admin@bencey.co.nz>
2022-03-22 11:37:33 +1300

    Update DomainCommands.ps1

--- level6/DomainCommands.ps1
+++ level6/DomainCommands.ps1
@@ -3,3 +3,9 @@ Add-Computer -DomainName adatum -Credential (Get-Credential) -NewName LON-SVR1 -
 
 # Add second Domain Controller
 Install-ADDSDomainController -domain contoso.local -SafeModeAdministratorPassword ("P@ssw0rd" | ConvertTo-SecureString -AsPlainText -force) -Credential (Get-Credential)
+
+# Create Forest
+Install-ADDSForest -DomainName vdom.local -InstallDNS
+
+## Add Domain User
+New-ADUser -Name "Benjamin Durham" -GivenName "Benjamin" -Surname "Durham" -SamAccountName "Ben" -UserPrincipalName "Ben@contoso.local" -AccountPassword(Read-Host -AsSecureString "Enter Password") -Enabled $true
