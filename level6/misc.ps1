b832485877f0d277389f146d73813acd3dab6506
Benjamin Durham <admin@bencey.co.nz>
2022-03-30 09:48:45 +1300

    Update misc.ps1

--- level6/misc.ps1
+++ level6/misc.ps1
@@ -3,3 +3,6 @@ New-VMSwitch NAT -SwitchType Internal
 Get-NetAdapter (To Get interface Index)
 
 New-NetIPAddress -InterfaceIndex (From above) -IPAddress 172.16.0.1 -PrefixLength 24
+
+New-netnat -Name NAT -InternalIPInterfaceAddressPrefix "172.16.0.0/24"
+Add-NetNatStaticMapping -NatName NAT -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 
